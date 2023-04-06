a61e0937e7f9dbca2731d02ab81792bae2eed0ea
eunsu02 <regulus_leo08@naver.com>
2023-04-06 18:51:33 +0900

    5주차 과제

--- Django/config/urls.py
+++ Django/config/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls')),
+    path('posts/', include('posts.urls')),
     path('introduction/',include('introduction.urls')),
 ]
